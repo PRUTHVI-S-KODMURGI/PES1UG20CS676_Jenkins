3b0764bc7ed59aa533b9243fb3bb2bc8c782ac86
Pruthvi S Kodmurgi <85472026+PRUTHVI-S-KODMURGI@users.noreply.github.com>
2023-02-17 10:17:45 +0530

    Update and rename hello.cpp to sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Final code!";
   cout << "I am Pruthvi S Kodmurgi\n";
   return 0;
 }
